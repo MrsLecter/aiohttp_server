60f78588b44cb64a4732933b97afabee493fe5da
@MrsLecter <54142283+MrsLecter@users.noreply.github.com>
2022-06-28 12:03:05 +0300

    add comments

--- index.py
+++ index.py
@@ -4,12 +4,15 @@ import tornado.web
 import motor.motor_tornado
 from bson import ObjectId
 import json
-
+# existing collectionsa
 collections = ['goods', 'purhases', 'users_basket']
 
+# connection string to mongo db
 connection_string = 'mongodb+srv://<login>:<passwd>@cluster0.2vrmo.mongodb.net/user_shopping_list?retryWrites=true&w=majority'
 
+# create motor client
 client = motor.motor_tornado.MotorClient(connection_string)
+
 db = client['user_shopping_list']
 
 
@@ -20,26 +23,27 @@ class JSONEncoder(json.JSONEncoder):
             return str(o)
         return json.JSONEncoder.default(self, o)
 
+# handle routes
 class MainHandler(tornado.web.RequestHandler):
-
+    
     async def get(self):
         collection_list = []
         for collection in collections:
             data = await self.settings["db"][collection].find().to_list(1000)
+            # json dumps for data object overcoming
             collection_list.append(JSONEncoder().encode(json.dumps(data, indent=4, sort_keys=True, default=str)))
-        # for time overcoming
         return self.write({"data": collection_list })
 
-
+# tornado web app
 app = tornado.web.Application(
     [
         (r"/", MainHandler),
-        (r"/(?P<student_id>\w+)", MainHandler),
     ],
     db=db,
 )
 
 if __name__ == "__main__":
+    # run tornado web app   
     app.listen(8000)
     # wrapper around the asyncio event loop
     tornado.ioloop.IOLoop.current().start()
